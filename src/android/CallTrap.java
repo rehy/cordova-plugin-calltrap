c1ec08ad2e5316d34fb9c95e8091f12be7ac0135
schlingel <der-schlingel@gmx.at>
2023-06-15 18:11:23 +0200

    feat(): Add support for SDK >= 31

--- src/android/CallTrap.java
+++ src/android/CallTrap.java
@@ -1,7 +1,13 @@
 package name.ratson.cordova.calltrap;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyCallback;
 import android.telephony.TelephonyManager;
 
 import org.apache.cordova.CallbackContext;
@@ -11,10 +17,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class CallTrap extends CordovaPlugin {
+    private static Logger log = Logger.getLogger(CallTrap.class.getName());
 
-    CallStateListener listener;
+    CallbackContextOwner listener;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -27,14 +37,88 @@ public class CallTrap extends CordovaPlugin {
 
     private void prepareListener() {
         if (listener == null) {
-            listener = new CallStateListener();
-            TelephonyManager TelephonyMgr = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-            TelephonyMgr.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+            try {
+                TelephonyManager telephonyMgr = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    CallTrapCallStateListener callStateListener = new CallTrapCallStateListener();
+
+                    listener = callStateListener;
+                    boolean listeningAllowed = ContextCompat.checkSelfPermission(cordova.getActivity().getApplicationContext(), Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED;
+
+                    if (listeningAllowed) {
+                        telephonyMgr.registerTelephonyCallback(
+                                cordova.getActivity().getMainExecutor(),
+                                callStateListener
+                        );
+                    }
+                } else {
+                    CallTrapPhoneStateListener phoneStateListener = new CallTrapPhoneStateListener();
+
+                    listener = phoneStateListener;
+                    telephonyMgr.listen(
+                            phoneStateListener,
+                            PhoneStateListener.LISTEN_CALL_STATE
+                    );
+                }
+            } catch (SecurityException se) {
+                log.log(Level.SEVERE, "Couldn't setup call trap listener due to Security Exception - no calltrap events will be triggered", se);
+            }
         }
     }
 }
 
-class CallStateListener extends PhoneStateListener {
+interface CallbackContextOwner {
+    void setCallbackContext(CallbackContext context);
+}
+
+@RequiresApi(api = Build.VERSION_CODES.S)
+class CallTrapCallStateListener extends TelephonyCallback implements TelephonyCallback.CallStateListener, CallbackContextOwner {
+    private CallbackContext callbackContext;
+
+    public void setCallbackContext(CallbackContext context) {
+        this.callbackContext = context;
+    }
+
+    public void onCallStateChanged(int state) {
+        if (callbackContext == null) {
+            return;
+        }
+
+        String msg = "";
+
+        switch (state) {
+            case TelephonyManager.CALL_STATE_IDLE:
+                msg = "IDLE";
+                break;
+
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                msg = "OFFHOOK";
+                break;
+
+            case TelephonyManager.CALL_STATE_RINGING:
+                msg = "RINGING";
+                break;
+        }
+
+        JSONObject jsonObj = new JSONObject();
+
+        try {
+            jsonObj.put("state", msg);
+            jsonObj.put("number", null);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+
+        PluginResult result = new PluginResult(PluginResult.Status.OK, jsonObj);
+        result.setKeepCallback(true);
+
+        callbackContext.sendPluginResult(result);
+    }
+}
+
+class CallTrapPhoneStateListener extends PhoneStateListener implements CallbackContextOwner {
 
     private CallbackContext callbackContext;
 
